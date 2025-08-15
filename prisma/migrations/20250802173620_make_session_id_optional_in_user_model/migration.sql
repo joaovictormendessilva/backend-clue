aaf4ce9925495e9e752b4e0598fa32f22b1b965b
joaovictormendessilva <joao_victor-08@hotmail.com>
2025-08-14 21:57:57 -0300

    Refactor database schema and migrations
    - Removed migrations for making session_id optional in user model and changing user-session relation.
    - Created new migration to establish a comprehensive database schema including users, sessions, session states, players, suggestions, cards, log events, revealed cards, and final accusations.
    - Updated schema.prisma to reflect new relationships and optional fields in User and SessionState models.
    - Introduced new Player model to manage user participation in session states.
    - Added enums for LogEventType, CardType, and SessionStateStatusType to enhance data integrity.

--- prisma/migrations/20250802173620_make_session_id_optional_in_user_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "users" DROP CONSTRAINT "users_session_id_fkey";
-
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "session_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_session_id_fkey" FOREIGN KEY ("session_id") REFERENCES "sessions"("id") ON DELETE SET NULL ON UPDATE CASCADE;
